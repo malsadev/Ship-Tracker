d17811624f71e0eb7781d94fc1337c7951d6eb94
Mustafa <Mustafa@10.0.0.244>
2021-05-12 04:29:47 -0400

    First commit

---
+++ Ship Tracker/src/PopulateScript.java
@@ -0,0 +1,17 @@
+/*
+ * Program Name: PopulateScript.java
+ * Purpose: Populates a specifically designed access database that keeps track of ships and their cargo (Aden)
+ * Author: Mustafa Al-Sakkaf
+ * Date: May 12, 2021
+ */
+
+
+public class PopulateScript {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World");
+
+	}
+
+}
