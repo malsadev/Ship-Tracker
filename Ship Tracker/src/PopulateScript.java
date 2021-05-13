b0fb00b89fedc69e60c72ed64786d0c0979104b1
Mustafa <Mustafa@10.0.0.244>
2021-05-12 23:29:19 -0400

    Final push of the day
    Remains to populate the database using a loop

--- Ship Tracker/src/PopulateScript.java
+++ Ship Tracker/src/PopulateScript.java
@@ -6,12 +6,60 @@
  */
 
 
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+import java.sql.*;
+
 public class PopulateScript {
 
-	public static void main(String[] args) {
-		//This is my second commit
-		System.out.println("Hello, World");
-
+	public static void main(String[] args) throws SQLException, FileNotFoundException {
+		
+		String databaseURL = "jdbc:ucanaccess://ShipTracker.accdb";
+		Connection connection = DriverManager.getConnection(databaseURL);
+		
+		File myFile = new File ("Aden Port Volume jan.csv");
+		
+		Scanner fileReader = new Scanner (myFile);
+		String  token = "";
+		
+		fileReader.useDelimiter("\n");
+		
+		while (fileReader.hasNext()) {
+			token = fileReader.next();
+			
+			if (token.contains("Jan")) {
+				token =  fileReader.next();
+				break;
+			}
+			//System.out.println(token);
+		}
+		
+		fileReader.useDelimiter(",");
+	    
+		//put all values in String arrayLists
+		ArrayList<String> shipNames = new ArrayList<String>();
+		ArrayList<String> ImoNums = new ArrayList<String>();
+		ArrayList<String> shipOwners = new ArrayList<String>();
+		
+		
+		while (fileReader.hasNext()) {
+			token = fileReader.next();
+			System.out.println(token);
+		}
+		
+		 String sql = "INSERT INTO Ship (Ship_Name, Imo_No, Owner) VALUES (?, ?, ?)";
+         
+         PreparedStatement preparedStatement = connection.prepareStatement(sql);
+         preparedStatement.setString(1, "Jim Rohn");
+         preparedStatement.setString(2, "rohnj@herbalife.com");
+         preparedStatement.setString(3, "0919989998");
+		
+		
+		
+		fileReader.close();
+		
 	}
 
 }
---
+++ Ship Tracker/src/ToolBox.java
@@ -0,0 +1,13 @@
+/*
+ * 
+ */
+
+
+public class ToolBox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
